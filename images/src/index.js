31ac4725151c974a550994a73f60c790d36b86bd
niko <7hesaint@gmail.com>
2022-10-18 22:48:13 +0300

    added JS, click changes picture

--- images/src/index.js
+++
@@ -1,3 +0,0 @@
-const anchors = document.querySelectorAll(".thumbnails-anchor");
-const detailsImage = document.querySelector(".details-image");
-const detailsTitle = document.querySelector(".details-title");
