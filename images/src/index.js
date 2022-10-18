b86d52e6d795dc47bae6953388c4af0ba228ed4c
niko <7hesaint@gmail.com>
2022-10-18 21:25:57 +0300

    index js

---
+++ images/src/index.js
@@ -0,0 +1,3 @@
+const anchors = document.querySelectorAll(".thumbnails-anchor");
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
